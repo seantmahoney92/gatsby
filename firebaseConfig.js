1cb09f0a6b8c72d8b53c5f193e690c60e5a2d51b
Sean Mahoney <sean@seantmahoney.com>
2019-09-19 17:01:13 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-  apiKey: '#',
-  authDomain: '#',
-  databaseURL: '#',
-  projectId: '#',
-  storageBucket: '#',
-  messagingSenderId: '#',
-  appId: '#',
+  apiKey: "AIzaSyCD6nvemCT8-uVDx6t-z6la1BDC3uECI2s",
+  authDomain: "gatsby-529.firebaseapp.com",
+  databaseURL: "https://gatsby-529.firebaseio.com",
+  projectId: "gatsby-529",
+  storageBucket: "gatsby-529.appspot.com",
+  messagingSenderId: "496318556308",
+  appId: "1:496318556308:web:9fe62ee2b120ea42dc2c81"
 };
